9e98b01405e5da0610788d00ed71d09760fab366
Renato Sousa <94645489+SousaExm@users.noreply.github.com>
2022-06-29 03:27:55 -0300

    requisitando dados da api e renderizando em tela na page home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { GlobalStyle } from '../styles/global'
 import { Layout } from './components/Layout'
  
